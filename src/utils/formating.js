69f724aca9b71ba8f97ff893eb7d20ba0091b55c
Deric <dericgf@gmail.com>
2019-11-26 23:59:05 -0700

    Add the reverse of toMoney function
    also added conditions to strip out commas and $ signs

--- src/utils/formating.js
+++ src/utils/formating.js
@@ -1,3 +1,20 @@
 export const toMoney = (unformatted) => {
-    return Number(unformatted).toLocaleString('en', {minimumFractionDigits:2, maximumFractionDigits:2, currency: "cad"})
+    if (String(unformatted).includes('$')) {
+        unformatted = String(unformatted).replace('$', "")
+    }
+    if (String(unformatted).includes(',')) {
+        unformatted = String(unformatted).replace(',', "")
+    }
+    return Number(unformatted).toLocaleString('en', { minimumFractionDigits: 2, maximumFractionDigits: 2, currency: "cad" })
+}
+
+
+export const fromMoney = (unformatted) => {
+    if (String(unformatted).includes('$')) {
+        unformatted = String(unformatted).replace('$', "")
+    }
+    if (String(unformatted).includes(',')) {
+        unformatted = String(unformatted).replace(',', "")
+    }
+    return String(unformatted)
 }
