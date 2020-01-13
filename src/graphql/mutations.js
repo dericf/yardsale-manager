1e29434cb1cfa748254b7f4955387ea5d1586429
deric92 <dericgf@gmail.com>
2020-01-12 23:12:13 -0700

    Format Mutations file

--- src/graphql/mutations.js
+++ src/graphql/mutations.js
@@ -1,14 +1,32 @@
-import gql from 'graphql-tag'
+import gql from "graphql-tag";
 /* 
   SELLERS 
 */
 
-// 
+//
 // Insert Seller
-// 
+//
 export const CREATE_SELLER = gql`
-mutation CreateSeller($email: String, $initials: String, $name: String, $phone: String, $address: String, $company: String, $notes: String) {
-    insert_seller(objects: {email: $email, initials: $initials, name: $name, address_text: $address, company: $company, notes: $notes, phone: $phone}) {
+  mutation CreateSeller(
+    $email: String
+    $initials: String
+    $name: String
+    $phone: String
+    $address: String
+    $company: String
+    $notes: String
+  ) {
+    insert_seller(
+      objects: {
+        email: $email
+        initials: $initials
+        name: $name
+        address_text: $address
+        company: $company
+        notes: $notes
+        phone: $phone
+      }
+    ) {
       returning {
         uuid
         user_uuid
@@ -19,114 +37,199 @@ mutation CreateSeller($email: String, $initials: String, $name: String, $phone: 
         is_user_link
       }
     }
-}`
+  }
+`;
 //
 // Update Seller
 //
 export const UPDATE_SELLER = gql`
-mutation updateSeller($sellerUUID: uuid!, $phone: String, $initials: String, $notes: String, $name: String, $email: String, $company: String, $address: String) {
-  update_seller(where: {uuid: {_eq: $sellerUUID}}, _set: {updated_at: "NOW()", initials: $initials, phone: $phone, notes: $notes, name: $name, email: $email, company: $company, address_text: $address}) {
-    returning {
-      uuid
-      updated_at
+  mutation updateSeller(
+    $sellerUUID: uuid!
+    $phone: String
+    $initials: String
+    $notes: String
+    $name: String
+    $email: String
+    $company: String
+    $address: String
+  ) {
+    update_seller(
+      where: { uuid: { _eq: $sellerUUID } }
+      _set: {
+        updated_at: "NOW()"
+        initials: $initials
+        phone: $phone
+        notes: $notes
+        name: $name
+        email: $email
+        company: $company
+        address_text: $address
+      }
+    ) {
+      returning {
+        uuid
+        updated_at
+      }
     }
   }
-}
-`
+`;
 
 //
 // Delete Seller
 //
 export const DELETE_SELLER = gql`
-mutation updateSeller($sellerUUID: uuid!) {
-  update_seller(where: {uuid: {_eq: $sellerUUID}}, _set: {is_deleted: true, is_active: false}) {
-    returning {
-      uuid
-      updated_at
+  mutation updateSeller($sellerUUID: uuid!) {
+    update_seller(
+      where: { uuid: { _eq: $sellerUUID } }
+      _set: { is_deleted: true, is_active: false }
+    ) {
+      returning {
+        uuid
+        updated_at
+      }
     }
   }
-}`
+`;
 
 /* 
   YARDSALES 
 */
 
-// 
+//
 // Insert Yardsale
-// 
+//
 export const CREATE_YARDSALE = gql`
-mutation CreateYardsale($email: String, $name: String, $phone: String, $address_text: String, $notes: String, $company: String) {
-    insert_yardsale(objects: {email: $email, name: $name, address_text: $address_text, notes: $notes, phone: $phone, company: $company}) {
+  mutation CreateYardsale(
+    $email: String
+    $name: String
+    $phone: String
+    $address_text: String
+    $notes: String
+    $company: String
+    $is_public: Boolean
+    $pos_lat: float8
+    $pos_lng: float8
+  ) {
+    insert_yardsale(
+      objects: {
+        email: $email
+        name: $name
+        address_text: $address_text
+        notes: $notes
+        phone: $phone
+        company: $company,
+        is_public: $is_public
+        pos_lat: $pos_lat
+        pos_lng: $pos_lng  
+      }
+    ) {
       returning {
         uuid
         name
       }
     }
-}`
+  }
+`;
 //
 // Update Yardsale
 //
 export const UPDATE_YARDSALE = gql`
-mutation updateYardsale($yardsaleUUID: uuid!, $phone: String, $notes: String, $name: String, $email: String $address_text: String, $company: String) {
-  update_yardsale(where: {uuid: {_eq: $yardsaleUUID}}, _set: {phone: $phone, notes: $notes, name: $name, email: $email, address_text: $address_text, company: $company}) {
-    returning {
-      uuid
-      name
+  mutation updateYardsale(
+    $yardsaleUUID: uuid!
+    $phone: String
+    $notes: String
+    $name: String
+    $email: String
+    $address_text: String
+    $company: String
+    $is_public: Boolean
+    $pos_lat: float8
+    $pos_lng: float8
+  ) {
+    update_yardsale(
+      where: { uuid: { _eq: $yardsaleUUID } }
+      _set: {
+        phone: $phone
+        notes: $notes
+        name: $name
+        email: $email
+        address_text: $address_text
+        company: $company,
+        is_public: $is_public
+        pos_lat: $pos_lat
+        pos_lng: $pos_lng
+      }
+    ) {
+      returning {
+        uuid
+        name
+      }
     }
   }
-}
-`
+`;
 
 //
 // Delete Yardsale
 //
 export const DELETE_YARDSALE = gql`
-mutation updateYardsale($yardsaleUUID: uuid!) {
-  delete_yardsale(where: {uuid: {_eq: $yardsaleUUID}}) {
-    returning {
-      uuid
+  mutation updateYardsale($yardsaleUUID: uuid!) {
+    delete_yardsale(where: { uuid: { _eq: $yardsaleUUID } }) {
+      returning {
+        uuid
+      }
     }
   }
-}`
-
-
+`;
 
 //
 // Transaction Item
 //
 export const CREATE_TRANSACTION_ITEM = gql`
-mutation CreateTransactionItem($sellerUUID: uuid, $description: String, $price: money, $yardsaleUUID: uuid!) {
-  insert_transaction(objects: {description: $description, price: $price, seller_uuid: $sellerUUID, yardsale_uuid: $yardsaleUUID}) {
-    returning {
-      uuid
+  mutation CreateTransactionItem(
+    $sellerUUID: uuid
+    $description: String
+    $price: money
+    $yardsaleUUID: uuid!
+  ) {
+    insert_transaction(
+      objects: {
+        description: $description
+        price: $price
+        seller_uuid: $sellerUUID
+        yardsale_uuid: $yardsaleUUID
+      }
+    ) {
+      returning {
+        uuid
+      }
     }
   }
-}`
-
+`;
 
 //
 // Create Yardsale Seller Link
 //
 export const CREATE_YARDSALE_SELLER_LINK = gql`
-mutation CreateYardsaleSellerLink($sellerUUID: uuid!, $yardsaleUUID: uuid!) {
-  insert_yardsale_seller_link(objects: {seller_uuid: $sellerUUID, yardsale_uuid: $yardsaleUUID}) {
-    returning {
-      uuid
+  mutation CreateYardsaleSellerLink($sellerUUID: uuid!, $yardsaleUUID: uuid!) {
+    insert_yardsale_seller_link(
+      objects: { seller_uuid: $sellerUUID, yardsale_uuid: $yardsaleUUID }
+    ) {
+      returning {
+        uuid
+      }
     }
   }
-}`
-
+`;
 
 //
 // Delete Yardsale Seller Link
 //
 export const DELETE_YARDSALE_SELLER_LINK = gql`
-mutation DeleteYardsaleSellerLink($UUID: uuid!) {
-  delete_yardsale_seller_link(where: {uuid: {_eq: $UUID}}) {
-    returning {
-      uuid
+  mutation DeleteYardsaleSellerLink($UUID: uuid!) {
+    delete_yardsale_seller_link(where: { uuid: { _eq: $UUID } }) {
+      returning {
+        uuid
+      }
     }
   }
-}
-`
-
+`;
