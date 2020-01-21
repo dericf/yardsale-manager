f6a92f19826968aff69d7e50a5928e6e2d8f8bee
deric92 <dericgf@gmail.com>
2020-01-20 20:34:36 -0700

    Fix yardsale Action Card

--- src/components/YardsaleActionCard/YardsaleActionCard.js
+++ src/components/YardsaleActionCard/YardsaleActionCard.js
@@ -1,86 +1,136 @@
-import React, { Component, Fragment } from 'react'
-import { Link, withRouter } from 'react-router-dom';
+import React, { Component, Fragment, useState } from "react";
+import { Link, withRouter } from "react-router-dom";
 import {
-    Header,
-    Icon,
-    Menu,
-    Responsive,
-    Button,
-    Grid
-} from 'semantic-ui-react'
+  Header,
+  Icon,
+  Menu,
+  Responsive,
+  Button,
+  Grid,
+  ButtonGroup,
+  Popup,
+  Segment
+} from "semantic-ui-react";
 
+import YardsaleDetailsModal from "../modals/YardsaleDetailsModal/YardsaleDetailsModal";
+import YardsaleTransactionsModal from "../modals/YardsaleTransactionsModal/YardsaleTransactionsModal";
+import YardsaleSellerModal from "../modals/YardsaleSellerModal/YardsaleSellerModal";
+import CashierModal from "../modals/CashierModal/CashierModal";
+import ConfirmModal from "../modals/generic/ConfirmModal";
 
-import YardsaleDetailsModal from '../modals/YardsaleDetailsModal/YardsaleDetailsModal'
-import YardsaleTransactionsModal from '../modals/YardsaleTransactionsModal/YardsaleTransactionsModal'
-import YardsaleSellerModal from '../modals/YardsaleSellerModal/YardsaleSellerModal'
-import CashierModal from '../modals/CashierModal/CashierModal'
-import ConfirmModal from '../modals/generic/ConfirmModal'
+import { notify } from "react-notify-toast";
+import { DELETE_YARDSALE } from "../../graphql/mutations";
+import { GET_YARDSALES } from "../../graphql/queries";
+import { useMutation } from "@apollo/react-hooks";
 
-import { notify } from 'react-notify-toast';
-import { DELETE_YARDSALE } from '../../graphql/mutations'
-import { GET_YARDSALES } from '../../graphql/queries'
-import { useMutation } from '@apollo/react-hooks'
+const DesktopActionGroup = ({
+  yardsale,
+  setCashierActive,
+  mobile = false,
+  desktop = true
+}) => {
+  // state for mobile hover menu
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(true);
+  //
+  const mobileStyles = {
+    paddingLeft: 0,
+    paddingRight: 0
+  };
+  return (
+    <Segment
+      compact
+      fluid
+      textAlign="center"
+      className="mx-auto rounded"
+      style={{ border: 0 }}
+    >
+      <Grid centered padded stackable>
+        
+        <Grid.Row centered className="py0">
+          <YardsaleDetailsModal
+            edit={true}
+            yardsale={yardsale}
+          />
 
-const YardsaleActions = ({ yardsale }) => {
+          <YardsaleSellerModal
+            yardsale={yardsale}
+          />
+          {(mobileMenuOpen || desktop) && (
+            <Fragment>
+              <YardsaleTransactionsModal
+                yardsale={yardsale}
+                iconLabel={false && "History"}
+              />
+              <ConfirmModal
+                circular={true}
+                buttonProps={{
+                  icon: "trash",
+                  content: false && "Remove",
+                  circular: true,
+                  className: "list-action-icon"
+                }}
+                handleConfirm={() => {
+                  confirmDeactivateYardsale();
+                }}
+                handleCancel={() => console.log("cancel")}
+                header="Confirm Delete"
+                content={`Proceed deleting ${yardsale.name}?`}
+                warningMessage={"Warning! This action cannot be undone!"}
+                popupEnabled={true}
+                popupMessage={"Remove Yardsale"}
+                popupPosition={"top center"}
+              />
+            </Fragment>
+          )}
+        </Grid.Row>
+      </Grid>
+    </Segment>
+  );
+};
 
-    const [deleteYardsaleMutation, { loading: deleteYardsaleLoading, error: deleteYardsaleError }] = useMutation(DELETE_YARDSALE, {
+const YardsaleActions = ({ yardsale, setCashierActive }) => {
+  const [
+    deleteYardsaleMutation,
+    { loading: deleteYardsaleLoading, error: deleteYardsaleError }
+  ] = useMutation(DELETE_YARDSALE, {});
 
-    })
+  const confirmDeactivateYardsale = () => {
+    console.log("Deleting Yardsale!!!");
+    deleteYardsaleMutation({
+      variables: {
+        yardsaleUUID: yardsale.uuid
+      },
+      refetchQueries: [
+        {
+          query: GET_YARDSALES
+        }
+      ]
+    });
+  };
 
-    const confirmDeactivateYardsale = () => {
-        deleteYardsaleMutation({
-            variables: {
-                yardsaleUUID: yardsale.uuid
-            },
-            refetchQueries: [{
-                query: GET_YARDSALES
-            }]
-        })
-    }
+  const cashierMode = () => {};
+  const props = {
+    yardsale,
+    setCashierActive,
+    confirmDeactivateYardsale
+  };
+  return (
+    <Fragment>
+      <Grid>
+        <Grid.Column width={16}>
+          <DesktopActionGroup {...props} />
 
-    const cashierMode = () => {
+          {/* <Responsive minWidth={650}>
+            <DesktopActionGroup {...props} desktop={true} mobile={null} />
+          </Responsive>
 
-    }
+          <Responsive maxWidth={649}>
+            <DesktopActionGroup {...props} mobile={true} desktop={null} />
+          </Responsive> */}
+        </Grid.Column>
+      </Grid>
+    </Fragment>
+  );
+};
 
-    return (
-        <Fragment>
-            <Grid>
-                <Grid.Row className="py0">
-                    <Grid.Column computer={8} mobile={16} style={{ paddingTop: 14 }}>
-                        <CashierModal iconLabel="New Sale" yardsale={yardsale} />
-                    </Grid.Column>
-
-                    <Grid.Column computer={8} mobile={16} style={{ paddingTop: 14 }}>
-                        <YardsaleDetailsModal edit={true} yardsale={yardsale} iconLabel="Edit" ></YardsaleDetailsModal>
-                    </Grid.Column>
-                </Grid.Row>
-                <Grid.Row className="py0">
-                    <Grid.Column computer={8} mobile={16} style={{ paddingTop: 14 }}>
-                        {/* <Button color="green" fluid ><Icon name="dollar"></Icon> Transaction History</Button> */}
-                        <YardsaleSellerModal yardsale={yardsale} iconLabel="Sellers" />
-                    </Grid.Column>
-                    <Grid.Column computer={8} mobile={16} style={{ paddingTop: 14 }}>
-                        {/* <Button color="green" fluid ><Icon name="dollar"></Icon> Transaction History</Button> */}
-                        <YardsaleTransactionsModal yardsale={yardsale} iconLabel="History" />
-                    </Grid.Column>
-                </Grid.Row>
-                <Grid.Row className="py0">
-                    <Grid.Column computer={16} mobile={16} style={{ paddingTop: 14 }}>
-                        <ConfirmModal
-                            buttonProps={{ icon: "trash", content: "Remove", fluid: true, negative: true }}
-                            handleConfirm={() => {
-                                confirmDeactivateYardsale()
-                            }}
-                            handleCancel={() => console.log('cancel')}
-                            header="Confirm Delete"
-                            content={`Proceed deleting ${yardsale.name}?`}
-                            warningMessage={"Warning! This action cannot be undone! Proceed with caution."}
-                        />
-                    </Grid.Column>
-                </Grid.Row>
-            </Grid>
-        </Fragment>
-    )
-}
-
-export default withRouter(YardsaleActions)
+export default withRouter(YardsaleActions);
