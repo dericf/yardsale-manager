eeb18f32db41a126cdfc7b341190483218d04060
deric92 <dericgf@gmail.com>
2020-01-20 20:34:52 -0700

    Cleanup Generic Loading Component

--- src/components/layout/Loading.js
+++ src/components/layout/Loading.js
@@ -4,13 +4,7 @@ import { Segment, Loader, Dimmer } from "semantic-ui-react";
 
 const Loading = () => (
 
-  <Segment loading  content="Loading" 
-    style={{
-        height: "100vh", 
-        width: "100vw",
-        position: "fixed"
-      }}
-    >
+  <Segment loading  content="Loading" id="LoadingSegment" className="rounded borderless">
   </Segment>
 );
 
