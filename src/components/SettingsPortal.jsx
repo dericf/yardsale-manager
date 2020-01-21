3d47a94887691823e161a911b748bbc1588c11c8
deric92 <dericgf@gmail.com>
2020-01-20 20:34:14 -0700

    Cleanup Settings Portal

--- src/components/SettingsPortal.jsx
+++ src/components/SettingsPortal.jsx
@@ -1,87 +1,100 @@
-import React, { useState, useContext } from "react";
-import { Menu, Icon, Portal, Segment, Button } from "semantic-ui-react";
-import { withRouter } from "react-router-dom";
-import { AppContext } from "../App";
+import React, { Fragment, useState, useContext } from "react";
+import {
+  Menu,
+  Icon,
+  Portal,
+  Segment,
+  Button,
+  Header,
+  Popup
+} from "semantic-ui-react";
+import { withRouter, Link } from "react-router-dom";
+import { AppContext, AuthContext } from "../App";
+import { GET_USER } from "../graphql/queries";
+import { useQuery } from "@apollo/react-hooks";
 
 const SettingsPortal = () => {
   const [settingsPortalOpen, setSettingsPortalOpen] = useState(false);
-  const { app, setApp } = useContext(AppContext)
+  const { auth, setAuth } = React.useContext(AuthContext);
+  const { app, setApp } = useContext(AppContext);
+
+  const openLoginModal = () => {
+    setApp({ ...app, showLoginModal: true });
+    console.log("Opened the modal");
+  };
+
+  const { loading: userLoading, error: userError, data: userData } = useQuery(
+    GET_USER,
+    {
+      onError: e => console.log("ERROR WITH Get User QUERY", e),
+      onCompleted: data => {
+        if (data) {
+          setAuth({ ...auth, user: data.user[0] });
+        }
+      }
+    }
+  );
   return (
     <Menu.Item>
       <Icon
         circular
-        fitted
         name="setting"
         className="sidebar-item"
         onClick={() => setSettingsPortalOpen(true)}
       ></Icon>
       {/* TODO: Move this to a component */}
       <Portal
-            onClose={() => setSettingsPortalOpen(false)}
-            open={settingsPortalOpen}
-          >
-            <Segment
-              id="SettingsPortal"
-              loading={userLoading}
-            >
+        onClose={() => setSettingsPortalOpen(false)}
+        open={settingsPortalOpen}
+      >
+        <Segment id="SettingsPortal" loading={userLoading}>
+          <Fragment>
+            {auth.user && (
               <Fragment>
-                {auth && auth.user && (
-                  <Fragment>
-                    <Header>Settings</Header>
-                    <p>
-                      User: {auth.user.email}
-                      {auth.user.has_confirmed && (
-                        <Popup
-                          trigger={
-                            <Icon name="check" color="green" size="small" />
-                          }
-                          content={"Verified Email"}
-                        />
-                      )}
-                    </p>
-                    <p>
-                      <Link
-                        to="/request-change-password"
-                        onClick={() => setSettingsPortalOpen(false)}
-                      >
-                        Change Password
-                      </Link>
-                    </p>
-                    {!auth.user.has_completed_onboarding && (
-                      <p>
-                        <Link
-                          to="/welcome"
-                          onClick={() => setSettingsPortalOpen(false)}
-                        >
-                          Click here to resume the onboarding process
-                        </Link>
-                      </p>
-                    )}
-                    <Button
-                      basic
-                      circular
-                      fluid
-                      icon="power off"
-                      content="Logout"
-                      onClick={() => setAuth(auth.logout(auth, setAuth))}
-                    />
-                  </Fragment>
+                <Header>Settings</Header>
+                <p className="ui text">User: {auth.user.email}</p>
+                <p>
+                  <Link
+                    to="/request-change-password"
+                    onClick={() => setSettingsPortalOpen(false)}
+                  >
+                    Change Password
+                  </Link>
+                </p>
+                {!auth.user.has_completed_onboarding && (
+                  <p>
+                    <Link
+                      to="/welcome"
+                      onClick={() => setSettingsPortalOpen(false)}
+                    >
+                      Click here to resume the onboarding process
+                    </Link>
+                  </p>
                 )}
-                {/* {JSON.stringify(auth.user)} */}
-              </Fragment>
-              {auth && !auth.user && (
                 <Button
+                  className="save"
                   fluid
                   icon="power off"
-                  content="Log In"
-                  onClick={openLoginModal}
+                  content="Logout"
+                  onClick={() => setAuth(auth.logout(auth, setAuth))}
                 />
-              )}
-            </Segment>
-          </Portal>
+              </Fragment>
+            )}
+            {/* {JSON.stringify(auth.user)} */}
+          </Fragment>
+          {!auth.user && (
+            <Button
+              fluid
+              className="save"
+              icon="power off"
+              content="Log In"
+              onClick={openLoginModal}
+            />
+          )}
+        </Segment>
+      </Portal>
     </Menu.Item>
   );
 };
 
-
-export default withRouter(SettingsPortal)
+export default withRouter(SettingsPortal);
