d054976d245dc88910243942c36bb8d01f0d1d56
deric92 <dericgf@gmail.com>
2021-02-05 01:24:18 -0700

    Implement use of Protected route on Yardsale list page

--- pages/yardsales/index.tsx
+++ pages/yardsales/index.tsx
@@ -1,5 +1,17 @@
 import { Segment } from "semantic-ui-react";
+import { Layout } from "../../components/layout/Layout";
+// import useSWR from "swr";
+import { ProtectedComponent } from "../../components/ProtectedComponent";
+import { useAuth } from "../../hooks/useAuth";
 
 export default function index() {
-  return <Segment raised>Yardsales List</Segment>;
+  const { user } = useAuth();
+  // const { data } = useSWR(["/api/posts", user]);
+  return (
+    <ProtectedComponent>
+      <Layout>
+        <Segment raised>Yardsales List</Segment>
+      </Layout>
+    </ProtectedComponent>
+  );
 }
