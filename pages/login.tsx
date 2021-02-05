734714d643b7f2c2f5382c53c30b51a4d9d31f86
deric92 <dericgf@gmail.com>
2021-02-05 01:25:04 -0700

    Implement auth checks for login and register pages

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,5 +1,27 @@
-import { Segment } from "semantic-ui-react";
+import { useRouter } from "next/router";
+import { Button, Header, Segment } from "semantic-ui-react";
+import { Layout } from "../components/layout/Layout";
+import { LoginForm } from "../components/LoginForm/LoginForm";
+import { useAuth } from "../hooks/useAuth";
 
 export default function index() {
-  return <Segment raised>Login</Segment>;
+  const { isAuthenticated } = useAuth();
+  const router = useRouter();
+
+  if (isAuthenticated === true) {
+    return (
+      <Layout>
+        <Segment basic textAlign="center">
+          <Header>You are already logged in</Header>{" "}
+          <Button onClick={() => router.back()}>Go Back</Button>
+        </Segment>
+      </Layout>
+    );
+  }
+
+  return (
+    <Layout>
+      <LoginForm />
+    </Layout>
+  );
 }
--- pages/register.tsx
+++ pages/register.tsx
@@ -1,5 +1,28 @@
-import { Segment } from "semantic-ui-react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { Button, Header, Segment } from "semantic-ui-react";
+import { Layout } from "../components/layout/Layout";
+import { LoginForm } from "../components/LoginForm/LoginForm";
+import { useAuth } from "../hooks/useAuth";
 
 export default function index() {
-  return <Segment raised>Signup</Segment>;
+  const { isAuthenticated } = useAuth();
+  const router = useRouter();
+
+  if (isAuthenticated === true) {
+    return (
+      <Layout>
+        <Segment basic textAlign="center">
+          <Header>You are already logged in</Header>{" "}
+          <Button onClick={() => router.back()}>Go Back</Button>
+        </Segment>
+      </Layout>
+    );
+  }
+
+  return (
+    <Layout>
+      <Header>Register</Header>
+    </Layout>
+  );
 }
