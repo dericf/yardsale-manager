4cedcc9ceac83775929cb574d23e22a9a571a30f
deric92 <dericgf@gmail.com>
2021-02-04 18:17:11 -0700

    Wrap _app around the basic context providers

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,23 @@
 import "semantic-ui-css/semantic.min.css";
 
+import AlertProvider from "../hooks/useAlert";
+import AuthProvider from "../hooks/useAuth";
+import LoadingProvider, { useIsLoading } from "../hooks/useIsLoading";
+// import DataProvider from "../hooks/useData";
+import { ToastProvider, useToasts } from "react-toast-notifications";
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ToastProvider>
+      <AlertProvider>
+        <LoadingProvider>
+          <AuthProvider>
+            <Component {...pageProps} />
+          </AuthProvider>
+        </LoadingProvider>
+      </AlertProvider>
+    </ToastProvider>
+  );
 }
 
 export default MyApp;
