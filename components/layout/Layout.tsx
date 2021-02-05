3c6ba92f0445634639abf30c62b9339c12d1455d
deric92 <dericgf@gmail.com>
2021-02-05 01:19:02 -0700

    Implement Main Layout Component

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,5 +1,29 @@
-import { Container } from "semantic-ui-react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { Container, Divider, Header, Segment } from "semantic-ui-react";
+import { useIsLoading } from "../../hooks/useIsLoading";
 
 export const Layout = (props) => {
-  return <Container>{props.children}</Container>;
+  const { loadingState } = useIsLoading();
+  const router = useRouter()
+  return (
+    <Container>
+      <Segment
+        raised
+        loading={loadingState && loadingState.isLoading}
+        style={{ height: "100vh" }}
+        padded
+      >
+        <Link href="/" as="/">
+          <Header as="h1" textAlign="center" >
+            {" "}
+            Yardsale Manager
+          </Header>
+        </Link>
+        <Divider />
+
+        {props.children}
+      </Segment>
+    </Container>
+  );
 };
