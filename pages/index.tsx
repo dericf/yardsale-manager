37f9a24ca380c9384fb1dfe23c0704b27074e3b8
deric92 <dericgf@gmail.com>
2021-02-05 01:24:38 -0700

    Implement a first draft of the home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,61 @@
-import { Segment } from "semantic-ui-react";
+import { NextPage } from "next";
+import Link from "next/link";
+import {
+  Button,
+  Divider,
+  Grid,
+  Header,
+  Item,
+  ItemGroup,
+  Menu,
+  Segment,
+} from "semantic-ui-react";
+import { Layout } from "../components/layout/Layout";
+import { useAuth } from "../hooks/useAuth";
+interface Props {}
 
-export default function index() {
-  return <Segment raised>Welcome</Segment>;
-}
+const index: NextPage<Props> = (props) => {
+  const {isAuthenticated, logout} = useAuth()
+  return (
+    <Layout>
+      <Segment padded basic>
+        <Grid centered>
+          <Grid.Column mobile={16} tablet={8} computer={12} floated="left">
+            <p className="ui text">Let's get you started</p>
+          </Grid.Column>
+          {!isAuthenticated && (
+            <Grid.Column
+            textAlign="right"
+            verticalAlign="middle"
+            mobile={16}
+            tablet={8}
+            computer={4}
+            floated="left"
+          >
+            <ItemGroup divided>
+              <Item>
+                <Link href="/login" as="/login">
+                  <Button fluid primary>
+                    Log In
+                  </Button>
+                </Link>
+              </Item>
+              <Item>
+                <Link href="/register" as="/register">
+                  <Button fluid>Register</Button>
+                </Link>
+              </Item>
+            </ItemGroup>
+          </Grid.Column>
+          )}
+
+          {isAuthenticated && (
+            <Button fluid secondary onClick={logout}>Logout</Button>
+          )}
+        </Grid>
+      </Segment>
+    </Layout>
+  );
+};
+
+export default index;
