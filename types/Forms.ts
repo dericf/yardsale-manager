84448027eba785ab345a82b94989629f73019342
deric92 <dericgf@gmail.com>
2021-02-05 22:31:12 -0700

    Start adding Form Interfaces

---
+++ types/Forms.ts
@@ -0,0 +1,12 @@
+interface LoginForm {
+  email: string;
+  password: string;
+}
+
+interface RegisterForm {
+  name: string;
+  email: string;
+  initials: string;
+  password: string;
+  confirmPassword: string;
+}
