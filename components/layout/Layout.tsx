4fa81783a2679be3dbf10da4ddf784895b0595fe
deric92 <dericgf@gmail.com>
2021-02-05 22:27:22 -0700

    Enhance Layout component

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,11 +1,39 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { Container, Divider, Header, Segment } from "semantic-ui-react";
+import { PropsWithChildren, useEffect } from "react";
+import {
+  Button,
+  Container,
+  Divider,
+  Header,
+  Menu,
+  Segment,
+} from "semantic-ui-react";
+import { useAuth } from "../../hooks/useAuth";
 import { useIsLoading } from "../../hooks/useIsLoading";
+import { ActivePage } from "../../types/General";
+import { NavBar } from "../NavBar/NavBar";
 
-export const Layout = (props) => {
+
+interface Props  {
+  activePage: ActivePage
+}
+
+export const Layout = (props: PropsWithChildren<Props>) => {
   const { loadingState } = useIsLoading();
-  const router = useRouter()
+  const { isAuthenticated, user, loadAuthStateFromLocalStorage } = useAuth();
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      console.log("Checking auth status.");
+      await loadAuthStateFromLocalStorage();
+    };
+
+    if (isAuthenticated === false) {
+      // Check if there is a saved token in localstorage
+      checkAuth();
+    }
+  }, []);
   return (
     <Container>
       <Segment
@@ -14,12 +42,19 @@ export const Layout = (props) => {
         style={{ height: "100vh" }}
         padded
       >
-        <Link href="/" as="/">
-          <Header as="h1" textAlign="center" >
-            {" "}
-            Yardsale Manager
-          </Header>
-        </Link>
+        <div className="flex row justify-between align-center wrap">
+          <div className="flex col">
+            <Link href="/" as="/">
+              <Header as="h1" textAlign="center" className="hover-pointer">
+                {" "}
+                Yard Sale Manager
+              </Header>
+            </Link>
+          </div>
+          <div className="flex col">
+            <NavBar activePage={props.activePage} />
+          </div>
+        </div>
         <Divider />
 
         {props.children}
