468327b7a10477cd9ff36b1bd682015c8fc16e8b
deric92 <dericgf@gmail.com>
2021-02-05 22:30:58 -0700

    Enhance the responce interfaces

--- types/RequestResponse.ts
+++ types/RequestResponse.ts
@@ -1,7 +1,17 @@
-export interface LoginResponse {
-	STATUS: 'OK' | 'ERROR',
-	MESSAGE: 'User not found' | 'Wrong password' | 'Email not confirmed',
-	token: string,
-	refreshToken: string,
-	user: any
+export interface GenericResponse {
+  STATUS: "OK" | "ERROR" | "UNAUTHORIZED";
+}
+
+export interface LoginResponse extends GenericResponse {
+  MESSAGE: "User not found" | "Wrong password" | "Email not confirmed";
+  token: string;
+  refreshToken: string;
+  user: any;
+}
+
+export interface RegisterResponse extends GenericResponse {
+  MESSAGE:
+    | "An account with that email already exists. Please try again."
+    | "An account with that email already exists. Please try again."
+    | "Email not confirmed";
 }
