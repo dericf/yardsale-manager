4d5105f81fe9f674c3adfe5f79905ab1b46954ce
deric92 <dericgf@gmail.com>
2021-02-05 22:30:06 -0700

    Enhance Register Next Page

--- pages/register.tsx
+++ pages/register.tsx
@@ -1,28 +1,29 @@
+import Head from "next/head";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { Button, Header, Segment } from "semantic-ui-react";
-import { Layout } from "../components/layout/Layout";
+import { Layout } from "../components/Layout/Layout";
 import { LoginForm } from "../components/LoginForm/LoginForm";
+import { RegisterForm } from "../components/Register/RegisterForm";
 import { useAuth } from "../hooks/useAuth";
 
 export default function index() {
   const { isAuthenticated } = useAuth();
   const router = useRouter();
 
-  if (isAuthenticated === true) {
-    return (
-      <Layout>
+  return (
+    <Layout activePage="register">
+      <Head>
+        <title>Register | Yard Sale Manager</title>
+      </Head>
+      {isAuthenticated === true ? (
         <Segment basic textAlign="center">
           <Header>You are already logged in</Header>{" "}
           <Button onClick={() => router.back()}>Go Back</Button>
         </Segment>
-      </Layout>
-    );
-  }
-
-  return (
-    <Layout>
-      <Header>Register</Header>
+      ) : (
+        <RegisterForm />
+      )}
     </Layout>
   );
 }
