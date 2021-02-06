2fb161b2cb0fa7754d82aa39b22f65ef00bba50a
deric92 <dericgf@gmail.com>
2021-02-05 22:27:58 -0700

    Bring in gql queries mutations and subscriptions

---
+++ graphql/mutations.js
@@ -0,0 +1,260 @@
+import gql from "graphql-tag";
+/*
+  USER
+*/
+//
+// Update User Onbording complete
+//
+export const UPDATE_USER_ONBOARDING = gql`
+  mutation setOnboardingComplete (
+    $UUID: uuid!
+  ) {
+    update_user(
+      where: { uuid: { _eq: $UUID } }
+      _set: {
+        has_completed_onboarding: true
+      }
+    ) {
+      returning {
+        uuid
+        has_completed_onboarding
+      }
+    }
+  }
+`;
+
+
+/* 
+  SELLERS 
+*/
+
+//
+// Insert Seller
+//
+export const CREATE_SELLER = gql`
+  mutation CreateSeller(
+    $email: String
+    $initials: String
+    $name: String
+    $phone: String
+    $address: String
+    $company: String
+    $notes: String
+  ) {
+    insert_seller(
+      objects: {
+        email: $email
+        initials: $initials
+        name: $name
+        address_text: $address
+        company: $company
+        notes: $notes
+        phone: $phone
+      }
+    ) {
+      returning {
+        uuid
+        user_uuid
+        name
+        is_active
+        email
+        created_at
+        is_user_link
+      }
+    }
+  }
+`;
+//
+// Update Seller
+//
+export const UPDATE_SELLER = gql`
+  mutation updateSeller(
+    $sellerUUID: uuid!
+    $phone: String
+    $initials: String
+    $notes: String
+    $name: String
+    $email: String
+    $company: String
+    $address: String
+  ) {
+    update_seller(
+      where: { uuid: { _eq: $sellerUUID } }
+      _set: {
+        updated_at: "NOW()"
+        initials: $initials
+        phone: $phone
+        notes: $notes
+        name: $name
+        email: $email
+        company: $company
+        address_text: $address
+      }
+    ) {
+      returning {
+        uuid
+        updated_at
+      }
+    }
+  }
+`;
+
+//
+// Delete Seller
+//
+export const DELETE_SELLER = gql`
+  mutation updateSeller($sellerUUID: uuid!) {
+    update_seller(
+      where: { uuid: { _eq: $sellerUUID } }
+      _set: { is_deleted: true, is_active: false }
+    ) {
+      returning {
+        uuid
+        updated_at
+      }
+    }
+  }
+`;
+
+/* 
+  YARDSALES 
+*/
+
+//
+// Insert Yardsale
+//
+export const CREATE_YARDSALE = gql`
+  mutation CreateYardsale(
+    $email: String
+    $name: String
+    $phone: String
+    $address_text: String
+    $notes: String
+    $company: String
+    $is_public: Boolean
+    $pos_lat: float8
+    $pos_lng: float8
+  ) {
+    insert_yardsale(
+      objects: {
+        email: $email
+        name: $name
+        address_text: $address_text
+        notes: $notes
+        phone: $phone
+        company: $company,
+        is_public: $is_public
+        pos_lat: $pos_lat
+        pos_lng: $pos_lng  
+      }
+    ) {
+      returning {
+        uuid
+        name
+      }
+    }
+  }
+`;
+//
+// Update Yardsale
+//
+export const UPDATE_YARDSALE = gql`
+  mutation updateYardsale(
+    $yardsaleUUID: uuid!
+    $phone: String
+    $notes: String
+    $name: String
+    $email: String
+    $address_text: String
+    $company: String
+    $is_public: Boolean
+    $pos_lat: float8
+    $pos_lng: float8
+  ) {
+    update_yardsale(
+      where: { uuid: { _eq: $yardsaleUUID } }
+      _set: {
+        phone: $phone
+        notes: $notes
+        name: $name
+        email: $email
+        address_text: $address_text
+        company: $company,
+        is_public: $is_public
+        pos_lat: $pos_lat
+        pos_lng: $pos_lng
+      }
+    ) {
+      returning {
+        uuid
+        name
+      }
+    }
+  }
+`;
+
+//
+// Delete Yardsale
+//
+export const DELETE_YARDSALE = gql`
+  mutation deleteYardsale($yardsaleUUID: uuid!) {
+    delete_yardsale(where: { uuid: { _eq: $yardsaleUUID } }) {
+      returning {
+        uuid
+      }
+    }
+  }
+`;
+
+//
+// Transaction Item
+//
+export const CREATE_TRANSACTION_ITEM = gql`
+  mutation CreateTransactionItem(
+    $sellerUUID: uuid
+    $description: String
+    $price: money
+    $yardsaleUUID: uuid!
+  ) {
+    insert_transaction(
+      objects: {
+        description: $description
+        price: $price
+        seller_uuid: $sellerUUID
+        yardsale_uuid: $yardsaleUUID
+      }
+    ) {
+      returning {
+        uuid
+      }
+    }
+  }
+`;
+
+//
+// Create Yardsale Seller Link
+//
+export const CREATE_YARDSALE_SELLER_LINK = gql`
+  mutation CreateYardsaleSellerLink($sellerUUID: uuid!, $yardsaleUUID: uuid!) {
+    insert_yardsale_seller_link(
+      objects: { seller_uuid: $sellerUUID, yardsale_uuid: $yardsaleUUID }
+    ) {
+      returning {
+        uuid
+      }
+    }
+  }
+`;
+
+//
+// Delete Yardsale Seller Link
+//
+export const DELETE_YARDSALE_SELLER_LINK = gql`
+  mutation DeleteYardsaleSellerLink($UUID: uuid!) {
+    delete_yardsale_seller_link(where: { uuid: { _eq: $UUID } }) {
+      returning {
+        uuid
+      }
+    }
+  }
+`;
---
+++ graphql/queries.js
@@ -0,0 +1,304 @@
+import gql from "graphql-tag";
+
+//
+// GET ONE USER
+// by email
+export const GET_USER = gql`
+  query GetUser {
+    user {
+      uuid
+      id
+      created_at
+      updated_at
+      email
+      confirmation_key
+      has_confirmed
+      first_name
+      last_name
+      role
+      has_completed_onboarding
+    }
+  }
+`;
+//
+// GET ALL SELLERS
+// (permission: for the current user)
+export const GET_SELLERS = gql`
+  query GetSellers {
+    seller(where: { is_deleted: { _eq: false } }, order_by: { name: asc }) {
+      address_text
+      email
+      initials
+      name
+      notes
+      phone
+      is_active
+      is_deleted
+      created_at
+      updated_at
+      user_uuid
+      uuid
+      company
+      is_user_link
+    }
+  }
+`;
+
+//
+// GET ALL SELLER LINKS FOR YARDSALE
+//
+export const GET_SELLER_LINKS_FOR_YARDSALE = gql`
+  query GetSellerLinksForYardsale($yardsaleUUID: uuid!) {
+    yardsale_seller_link(where: { yardsale_uuid: { _eq: $yardsaleUUID } }) {
+      uuid
+      yardsale_uuid
+      seller {
+        uuid
+        name
+        initials
+        is_active
+        is_deleted
+        transactions(where: { yardsale_uuid: { _eq: $yardsaleUUID } }) {
+          seller_uuid
+          yardsale_uuid
+          price
+          description
+        }
+      }
+    }
+  }
+`;
+
+//
+// GET ALL SELLER LINKS FOR SELLER
+//
+export const GET_SELLER_LINKS_FOR_SELLER = gql`
+  query GetSellerLinksForYardsale($sellerUUID: uuid!) {
+    yardsale_seller_link(where: { seller_uuid: { _eq: $sellerUUID } }) {
+      uuid
+      yardsale_uuid
+      yardsale {
+        uuid
+        name
+        transactions(where: { seller_uuid: { _eq: $sellerUUID } }) {
+          seller_uuid
+          yardsale_uuid
+          price
+          description
+        }
+      }
+    }
+  }
+`;
+
+//
+// GET ONE SELLER
+// by id
+export const GET_SELLER_BY_UUID = gql`
+  query GetSeller($uuid: uuid!) {
+    seller(where: { uuid: { _eq: $uuid } }) {
+      address_text
+      email
+      initials
+      name
+      notes
+      phone
+      is_active
+      is_deleted
+      created_at
+      updated_at
+      user_uuid
+      uuid
+      company
+      is_user_link
+    }
+  }
+`;
+//
+// GET ALL YARDSALES
+// (permission: for the current user)
+export const GET_YARDSALES = gql`
+  query GetYardsales {
+    yardsale(order_by: { created_at: desc }) {
+      uuid
+      created_at
+      updated_at
+      address_text
+      days_of_week
+      end_date
+      end_time
+      start_date
+      start_time
+      user_uuid
+      name
+      company
+      phone
+      email
+      address_text
+      notes
+      is_active
+      pos_lng
+      pos_lat
+      is_public
+      yardsale_seller_links {
+        seller {
+          name
+          email
+          is_active
+          is_deleted
+          uuid
+          phone
+          notes
+          initials
+          company
+          address_text
+        }
+      }
+    }
+  }
+`;
+//
+// PREVIEW: GET ALL YARDSALES
+// (permission: for the current user)
+export const GET_YARDSALES_PREVIEW = gql`
+  query GetYardsales {
+    yardsale(order_by: { updated_at: desc }) {
+      uuid
+      updated_at
+      address_text
+      user_uuid
+      name
+      company
+      phone
+      email
+      notes
+      is_active
+    }
+  }
+`;
+//
+// GET ONE YARDSALE
+// by id
+export const GET_YARDSALE = gql`
+  query GetYardsale($yardsaleUUID: uuid) {
+    yardsale(where: { uuid: { _eq: $yardsaleUUID } }) {
+      address_text
+      days_of_week
+      end_date
+      end_time
+      start_date
+      start_time
+      user_uuid
+      uuid
+      name
+      company
+      phone
+      email
+      address_text
+      notes
+      is_active
+      pos_lng
+      pos_lat
+      is_public
+      yardsale_seller_links {
+        seller {
+          name
+          email
+          is_active
+          is_deleted
+          uuid
+          phone
+          notes
+          initials
+          company
+          address_text
+        }
+      }
+    }
+  }
+`;
+
+//
+// GET ALL SALE ITEMS
+// by seller.id
+export const GET_TRANSACTION_ITEM = gql`
+  query GetTransactionItem($UUID: uuid!) {
+    transaction(where: { _eq: { uuid: $UUID } }) {
+      seller_uuid
+      created_at
+      description
+      price
+      uuid
+      yardsale_uuid
+      seller {
+        name
+        initials
+        email
+      }
+      yardsale {
+        name
+        address_text
+      }
+    }
+  }
+`;
+//
+// GET ALL SALE ITEMS FOR YARDSALE
+// by yardsale.id
+export const GET_TRANSACTION_ITEMS_FOR_YARDSALE = gql`
+  query GetTransactionItem($yardsaleUUID: uuid!) {
+    transaction(where: { yardsale_uuid: { _eq: $yardsaleUUID } }) {
+      seller_uuid
+      created_at
+      description
+      price
+      uuid
+      yardsale_uuid
+      seller {
+        uuid
+        is_active
+        is_deleted
+        name
+        initials
+        email
+        transactions(where: { yardsale_uuid: { _eq: $yardsaleUUID } }) {
+          price
+          description
+          yardsale_uuid
+        }
+      }
+    }
+  }
+`;
+//
+// GET ALL SALE ITEMS FOR SELLER
+// by yardsale.id
+export const GET_TRANSACTION_ITEMS_FOR_SELLER = gql`
+  query GetTransactionItem($sellerUUID: uuid!) {
+    transaction(where: { seller_uuid: { _eq: $sellerUUID } }) {
+      uuid
+      seller_uuid
+      yardsale_uuid
+      price
+      description
+      created_at
+      yardsale {
+        uuid
+        is_active
+        name
+      }
+      seller {
+        uuid
+        is_active
+        is_deleted
+        name
+        initials
+        email
+      }
+    }
+  }
+`;
+
+//
+// GET SALE ITEM
+// by id
