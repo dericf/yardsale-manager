eaa7e90c7b2898187a08504df90bee435ab7c66e
deric92 <dericgf@gmail.com>
2021-02-05 22:27:31 -0700

    Add proper NavBar Component

---
+++ components/NavBar/NavBar.tsx
@@ -0,0 +1,56 @@
+import { NextPage } from "next";
+import { useRouter } from "next/router";
+import { PropsWithChildren } from "react";
+import { Menu } from "semantic-ui-react";
+import { useAuth } from "../../hooks/useAuth";
+import { ActivePage } from "../../types/General";
+
+interface Props {
+  activePage: ActivePage;
+}
+
+export const NavBar: NextPage = ({ activePage }: PropsWithChildren<Props>) => {
+  const router = useRouter();
+  const { isAuthenticated, logout } = useAuth();
+
+  return (
+    <div className="flex row justify-between align-center">
+      <Menu pointing secondary>
+        <Menu.Item
+          name="yard sales"
+          onClick={async (e) => {
+            e.preventDefault;
+            router.push("/yardsales");
+          }}
+					active={activePage === 'yard sales'}
+        />
+        <Menu.Item
+          name="sellers"
+          onClick={async (e) => {
+            e.preventDefault;
+            router.push("/sellers");
+          }}
+					active={activePage === 'sellers'}
+        />
+        <Menu.Item
+          name="account"
+          onClick={async (e) => {
+            e.preventDefault;
+            router.push("/account");
+          }}
+					active={activePage === 'account'}
+        />
+        <Menu.Menu position="right">
+          <Menu.Item
+            name="logout"
+            onClick={async (e) => {
+              e.preventDefault;
+              await logout();
+              router.push("/");
+            }}
+          />
+        </Menu.Menu>
+      </Menu>
+    </div>
+  );
+};
