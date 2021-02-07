979582ff36c8721c18f065efc7fb5894941d4d9f
deric92 <dericgf@gmail.com>
2021-02-06 23:52:03 -0700

    Major updates to Yardsale List component and page

---
+++ components/YardSales/YardsaleList.tsx
@@ -0,0 +1,119 @@
+import React, {
+  Component,
+  Fragment,
+  PropsWithChildren,
+  useEffect,
+  useState,
+} from "react";
+import { Card, Icon, Divider, Grid, Header, Segment } from "semantic-ui-react";
+import { useYardsales } from "../../hooks/useYardsales";
+import { YardSalesInterface } from "../../types/YardSales";
+
+interface Props {}
+
+export const YardSaleList = () => {
+  // TODO: (Future) make this more responsive. Add more than just two widths
+  const { yardSales, updateYardSales } = useYardsales();
+  useEffect(() => {
+    console.log("YARDSALES: ");
+    console.log(yardSales);
+  }, []);
+  if (yardSales && yardSales?.length > 0) {
+    return (
+      <>
+        {yardSales?.map((yardSale: YardSalesInterface) => (
+          <Segment
+            key={yardSale.uuid}
+            textAlign="left"
+            fluid="true"
+            compact
+            style={{ border: "0px !important", width: "100%" }}
+          >
+            <Card>
+              <Card.Content>
+                <Card.Header as="h3" className="mb0">
+                  {yardSale.uuid}
+                </Card.Header>
+                <Card.Meta as="h6">{yardSale.company}</Card.Meta>
+                {/* <Card.Header as="h5" className="m0"><strong>Status:</strong> {(yardSale.is_active === true) && (<Icon name="genderless" title="Active" className="active-icon" color="green"></Icon>)}{(yardSale.is_active === false) && (<Icon name="genderless" color="red" title="Inactive" className="deactivated-icon"></Icon>)}</Card.Header> */}
+              </Card.Content>
+            </Card>
+            <Grid>
+              <Grid.Row>
+                <Grid.Column mobile={8} tablet={8} computer={10}>
+                  <Grid.Row verticalAlign="top" className="pb0">
+                    <Grid.Column computer={6} mobile={16} verticalAlign="top">
+                      <Card.Content>
+                        <Card.Header as="h3" className="mb0">
+                          {yardSale.name}
+                        </Card.Header>
+                        <Card.Meta as="h6">{yardSale.company}</Card.Meta>
+                        {/* <Card.Header as="h5" className="m0"><strong>Status:</strong> {(yardSale.is_active === true) && (<Icon name="genderless" title="Active" className="active-icon" color="green"></Icon>)}{(yardSale.is_active === false) && (<Icon name="genderless" color="red" title="Inactive" className="deactivated-icon"></Icon>)}</Card.Header> */}
+                      </Card.Content>
+                    </Grid.Column>
+                  </Grid.Row>
+
+                  {(yardSale.notes.length > 0 ||
+                    yardSale.address_text.length > 0) && (
+                    <Divider className="my0 m0" fitted></Divider>
+                  )}
+                  <Grid.Row className="py0">
+                    <Grid.Column computer={10} mobile={16}>
+                      <Card.Content>
+                        {yardSale.address_text &&
+                          yardSale.address_text.length > 1 && (
+                            <Card.Description>
+                              {String(yardSale.address_text)
+                                .split("\n")
+                                .map((line, index) => (
+                                  <Fragment key={`line1${index}`}>
+                                    {line} <br />
+                                  </Fragment>
+                                ))}
+                            </Card.Description>
+                          )}
+                      </Card.Content>
+                    </Grid.Column>
+                  </Grid.Row>
+
+                  <Grid.Row className="pt0">
+                    <Grid.Column computer={10} mobile={16}>
+                      <Card.Content>
+                        {yardSale.notes && yardSale.notes.length > 1 && (
+                          <Card.Description>
+                            <strong>Notes: </strong>
+                            {String(yardSale.notes)
+                              .split("\n")
+                              .map((line, index) => (
+                                <Fragment key={`line2${index}`}>
+                                  {line} <br />
+                                </Fragment>
+                              ))}
+                          </Card.Description>
+                        )}
+                      </Card.Content>
+                    </Grid.Column>
+                  </Grid.Row>
+                </Grid.Column>
+                <Grid.Column
+                  mobile={8}
+                  tablet={8}
+                  computer={6}
+                  textAlign="right"
+                  verticalAlign="middle"
+                >
+                  {/* <YardsaleActionCard
+                  yardSale={yardSale}
+                  setCashierActive={setCashierActive}
+                /> */}
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+          </Segment>
+        ))}
+      </>
+    );
+  } else {
+    return <div>No Yard Sales</div>;
+  }
+};
--- pages/yardsales/index.tsx
+++ pages/yardsales/index.tsx
@@ -1,13 +1,53 @@
+import {
+  ApolloClient,
+  HttpLink,
+  InMemoryCache,
+  split,
+  useQuery,
+  useApolloClient,
+} from "@apollo/client";
+import { getMainDefinition } from "@apollo/client/utilities";
+import { setContext } from "apollo-link-context";
+import jwtDecode from "jwt-decode";
+import { GetServerSideProps } from "next";
 import Head from "next/head";
-import { Header, Segment } from "semantic-ui-react";
+import Link from "next/link";
+import { Fragment, useEffect } from "react";
+import { Button, Divider, Grid, Header, Segment } from "semantic-ui-react";
 import { Layout } from "../../components/Layout/Layout";
 // import useSWR from "swr";
 import { ProtectedComponent } from "../../components/ProtectedComponent";
+import { GET_YARDSALES } from "../../graphql/queries";
 import { useAuth } from "../../hooks/useAuth";
+import { useHasura } from "../../hooks/useHasura";
+import { useYardsales } from "../../hooks/useYardsales";
+import { YardSalesContextInterFace } from "../../types/Context";
+import {YardSaleList} from '../../components/YardSales/YardsaleList'
+import { YardSalesInterface } from "../../types/YardSales";
 
 export default function index() {
-  const { user } = useAuth();
+  const { user, token } = useAuth();
   // const { data } = useSWR(["/api/posts", user]);
+  const {
+    yardSales,
+    updateYardSales,
+    selectedYardSale,
+    filter,
+    updateFilterText,
+    setYardSales,
+  } = useYardsales();
+  // const {client} = useHasura()
+  // const { loading, error, data } = useQuery({query: GET_YARDSALES});
+
+  const {mutation, query} = useHasura()
+  // const { query } = useApolloClient();
+  useEffect(() => {
+    console.log("Getting YardSales");
+    (async () => {
+      await updateYardSales()
+    })()
+  }, []);
+
   return (
     <ProtectedComponent>
       <Head>
@@ -17,7 +57,122 @@ export default function index() {
         <Header textAlign="center" as="h2">
           Yard Sales List
         </Header>
+
+        {/* {cashierActive && data && data.yardsale && (
+          <CashierModal
+              yardsale={cashierActive}
+              autoOpen={true}
+              setCashierActive={setCashierActive}
+            />
+          )} */}
+
+        <Grid columns={2} centered className="m0 p0">
+          <Grid.Row className="py0">
+            {/* First Grid.Row (Filters/Buttons) */}
+            <Grid.Column
+              verticalAlign="middle"
+              mobile={8}
+              tablet={8}
+              computer={10}
+              className="pl0"
+            >
+              {/* Radio Buttons + Search Field */}
+              {/* <YardsalesFilterForm
+                  filter={filter}
+                  setFilter={setFilter}
+                  autofocus={true}
+                /> */}
+                <Link href='/yardsales/new' as="/yardsales/new"><Button>New</Button></Link>
+            </Grid.Column>
+            <Grid.Column
+              mobile={8}
+              tablet={8}
+              computer={6}
+              textAlign="right"
+              className="mobile-my8 pr0"
+            >
+              {/* <YardsaleDetailsM`odal /> */}
+            </Grid.Column>
+          </Grid.Row>
+
+          <Grid.Row className="pb0 pt0">
+            {/* Second Grid.Row (Dividers with headings) */}
+            <Grid.Column computer={16}>
+              <Divider>{`My Yard Sales`}</Divider>
+              {/* {JSON.stringify(error || "error")} */}
+              {/* {JSON.stringify(loading || "loading")} */}
+              {/* {JSON.stringify(data || "data")} */}
+            </Grid.Column>
+            {/* <Grid.Column mobile={8} tablet={7} computer={6}>
+                            <Divider horizontal={true} content="Actions"></Divider>
+                        </Grid.Column> */}
+          </Grid.Row>
+
+          {/* Yardsales List */}
+          {/* {loading && <Loading />} */}
+          {/* <pre>{JSON.stringify(yardSales, null, 4)}</pre> */}
+          <YardSaleList></YardSaleList>
+        </Grid>
       </Layout>
     </ProtectedComponent>
   );
 }
+
+// export const getServerSideProps: GetServerSideProps = async (context) => {
+//   console.log("req. :>> ", context.query);
+//   // const {token} = useAuth()
+
+//   return {
+//     props: {
+//     },
+//   };
+// };
+
+/**
+ * 
+ * {!loading &&
+            data &&
+            data.yardsale &&
+            data.yardsale
+              .filter((yardsale) => {
+                return (
+                  filter.searchText === "" // ||
+                  // (filter.searchText !== "" &&
+                  //   (String(yardsale.name)
+                  //     .toLowerCase()
+                  //     .includes(filter.searchText.toLowerCase()) ||
+                  //     String(yardsale.company)
+                  //       .toLowerCase()
+                  //       .includes(filter.searchText.toLowerCase()) ||
+                  //     String(yardsale.address_text)
+                  //       .toLowerCase()
+                  //       .includes(filter.searchText.toLowerCase())) &&
+                  //   (filter.status === "all" ||
+                  //     (filter.status === "active" &&
+                  //       yardsale.is_active === true) ||
+                  //     (filter.status === "inactive" &&
+                  //       yardsale.is_active === false)))
+                );
+              })
+              .map((yardsale) => {
+                return (
+                  <Grid.Row key={yardsale.uuid}>
+                    <Grid.Column width={12} textAlign="center">
+                      <Fragment>
+                          <p
+                          style={{
+                            width: "100px",
+                            height: "70px",
+                            border: "solid 1px #000",
+                          }}
+                        >
+                          Yardsale Card
+                        </p>
+                      </Fragment>
+                    </Grid.Column>
+
+                    
+                  </Grid.Row>
+                );
+              })}
+ */
