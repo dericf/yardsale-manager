7a250183ce5608edc2881da021d2781c1dfacb1c
deric92 <dericgf@gmail.com>
2021-02-09 22:54:10 -0700

    Implement new functionality in useYardsales context

--- hooks/useYardsales.tsx
+++ hooks/useYardsales.tsx
@@ -1,15 +1,29 @@
 // https://hasura.io/learn/graphql/nextjs-fullstack-serverless/queries/2-create-query/
 import { useState, useContext, useEffect, createContext } from "react";
-import { GET_YARDSALES } from "../graphql/queries";
-import { CREATE_YARDSALE, UPDATE_YARDSALE } from "../graphql/mutations";
+import {
+  GET_SELLER_LINKS_FOR_YARDSALE,
+  GET_TRANSACTION_ITEMS_FOR_YARDSALE,
+  GET_YARDSALES,
+} from "../graphql/queries";
+import {
+  CREATE_TRANSACTION_ITEM,
+  CREATE_YARDSALE,
+  CREATE_YARDSALE_SELLER_LINK,
+  DELETE_YARDSALE,
+  DELETE_YARDSALE_SELLER_LINK,
+  UPDATE_YARDSALE,
+} from "../graphql/mutations";
 import React from "react";
-import { useQuery } from "@apollo/client";
 import HasuraProvider, { useHasura } from "./useHasura";
 import { UseQueryResponse } from "../types/GraphQL";
 import { YardSalesContextInterFace } from "../types/Context";
 import { YardSalesInterface } from "../types/YardSales";
 import { useAuth } from "./useAuth";
 import { FormValues } from "./useForm";
+import { SellersInterface } from "../types/Sellers";
+import { UUID } from "../types/General";
+import { YardSaleLinks } from "../types/YardSaleLinks";
+import { Transaction } from "../types/Transaction";
 
 interface ApolloQueryReturn {
   loading: boolean;
@@ -28,13 +42,15 @@ export const YardSalesContext = createContext<YardSalesContextInterFace>(
 
 export default function YardSalesProvider({ children }) {
   const { user } = useAuth();
-  const { query } = useHasura();
+  const { query, mutation } = useHasura();
 
-  const [yardSales, setYardSales] = useState(
-    new Array<YardSalesInterface>(),
+  const [yardSales, setYardSales] = useState(new Array<YardSalesInterface>());
+  const [selectedYardSale, setSelectedYardSale] = useState<YardSalesInterface>(
+    null,
   );
-  const [selectedYardSale, setSelectedYardSale] = useState<YardSalesInterface>(null);
   // const { client } = useHasura();
+  const [sellerLinks, setSellerLinks] = useState(new Array<YardSaleLinks>());
+  const [transactionItems, setTransactionItems] = useState(new Array<Transaction>());
 
   const [filter, setFilter] = useState({
     searchText: "",
@@ -52,9 +68,13 @@ export default function YardSalesProvider({ children }) {
     // Make query to update all yardsales for this user
     const data = await query(GET_YARDSALES);
     console.log("Query...");
-    setYardSales(data.yardsale)
+    setYardSales(data.yardsale);
     console.log(data);
-    return { data: data.yardsale, loading: false, error: false } as UseQueryResponse;
+    return {
+      data: data.yardsale,
+      loading: false,
+      error: false,
+    } as UseQueryResponse;
   };
 
   const createNewYardsale = async (yardsale) => {
@@ -70,11 +90,91 @@ export default function YardSalesProvider({ children }) {
     return responseData;
   };
 
+  const deleteYardSale = async (yardSaleId: string) => {
+    // post to graphql -> mutation insertYardsale
+    const responseData = await query(DELETE_YARDSALE, {
+      yardsaleUUID: yardSaleId,
+    });
+    console.log("\n\nResponse from delete was..");
+    console.log(responseData);
+    setYardSales({ ...yardSales.filter((ys) => ys.uuid !== yardSaleId) });
+    return responseData;
+  };
+
+  const deleteYardSaleSellerLink = async (linkId: UUID): Promise<any> => {
+    const { data } = await query(DELETE_YARDSALE_SELLER_LINK, {
+      UUID: linkId,
+    });
+    
+    return data;
+  };
+
+  const getAllYardSaleSellerLinks = async (yardSaleId: UUID) => {
+    const {yardsale_seller_link: data} = await query(GET_SELLER_LINKS_FOR_YARDSALE, {
+      yardsaleUUID: yardSaleId,
+    });
+    console.log("Seller Links");
+    console.log(data);
+    setSellerLinks(data);
+    return data;
+  };
+
+  const createYardSaleSellerLink = async (sellerId: UUID, yardSaleId: UUID) => {
+    const { data } = await query(CREATE_YARDSALE_SELLER_LINK, {
+      sellerUUID: sellerId,
+      yardsaleUUID: yardSaleId,
+    });
+    return data;
+  };
+
+  const getSellersCanBeAdded = (
+    allSellers: Array<SellersInterface>,
+    existingSellerLinks: Array<YardSaleLinks>,
+  ) => {
+    return allSellers.filter((seller) => {
+      let linkExists = false;
+      existingSellerLinks?.forEach((link) => {
+        if (link.seller.uuid === seller.uuid) {
+          linkExists = true;
+        }
+      });
+      return !linkExists;
+    });
+  };
+
+  /**
+   * Transactions
+   */
+
+  const getAllYardSaleTransactions = async (yardSaleId: UUID) => {
+    const {yardsale_transaction: data} = await query(GET_TRANSACTION_ITEMS_FOR_YARDSALE, {
+      yardsaleUUID: yardSaleId,
+    });
+    console.log("\nTransactions: ");
+    console.log(data);
+    setTransactionItems(data);
+    return data;
+  };
+
+  const createYardSaleTransaction = async (yardSaleId: UUID, transactionItem: Transaction) => {
+    const {yardsale_transaction: data} = await query(CREATE_TRANSACTION_ITEM, {
+      sellerUUID: transactionItem.seller.uuid,
+      yardsaleUUID: yardSaleId,
+      price: String(transactionItem.price),
+      description: transactionItem.description,
+    });
+    console.log("\nTransactions: ");
+    console.log(data);
+    setTransactionItems(data);
+    return data;
+  }
   return (
     <YardSalesContext.Provider
       value={{
         yardSales,
         selectedYardSale,
+        sellerLinks,
+        transactionItems,
         setSelectedYardSale,
         updateYardSales,
         setYardSales,
@@ -82,6 +182,13 @@ export default function YardSalesProvider({ children }) {
         filter,
         createNewYardsale,
         updateYardsale,
+        deleteYardSale,
+        deleteYardSaleSellerLink,
+        getAllYardSaleSellerLinks,
+        createYardSaleSellerLink,
+        getSellersCanBeAdded,
+        getAllYardSaleTransactions,   
+        createYardSaleTransaction,     
       }}
     >
       {children}
