5111e6c419702268e9c237d91d3a23794f505270
deric92 <dericgf@gmail.com>
2021-02-10 17:15:10 -0700

    Render SellersList component on sellers page

--- pages/sellers/index.tsx
+++ pages/sellers/index.tsx
@@ -1,20 +1,33 @@
-import Head from 'next/head'
+import Head from "next/head";
+import { useEffect } from "react";
 import { Header, Segment } from "semantic-ui-react";
 import { Layout } from "../../components/Layout/Layout";
 // import useSWR from "swr";
 import { ProtectedComponent } from "../../components/ProtectedComponent";
+import { SellersList } from "../../components/Sellers/SellersList";
 import { useAuth } from "../../hooks/useAuth";
+import { useSellers } from "../../hooks/useSeller";
 
 export default function index() {
   const { user } = useAuth();
+  const { sellers, updateSellers } = useSellers();
   // const { data } = useSWR(["/api/posts", user]);
+  useEffect(() => {
+    (async () => {
+      await updateSellers();
+    })();
+  }, []);
+
   return (
     <ProtectedComponent>
       <Head>
         <title>Sellers | Yard Sale Manager</title>
       </Head>
       <Layout activePage="sellers">
-        <Header textAlign="center" as="h2">Sellers List</Header>
+        <Header textAlign="center" as="h2">
+          Sellers List
+        </Header>
+        <SellersList />
       </Layout>
     </ProtectedComponent>
   );
