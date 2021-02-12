9819a0aa870992e5f8b0cb2b9b800e118cf57d3d
deric92 <dericgf@gmail.com>
2021-02-11 18:19:16 -0700

    Major overhaul to sellers list

--- components/Sellers/SellersList.tsx
+++ components/Sellers/SellersList.tsx
@@ -16,8 +16,12 @@ import {
   Segment,
   Button,
 } from "semantic-ui-react";
+import { useAlert } from "../../hooks/useAlert";
+import { useAuth } from "../../hooks/useAuth";
 import { useSellers } from "../../hooks/useSeller";
 import { SellersInterface } from "../../types/Sellers";
+import { ConfirmModal } from "../ConfirmModal";
+import { SellerTransactionModal } from "../Modals/SellerTransactionModal";
 import { SellersFilterForm } from "./SellersFilterForm";
 
 interface Props {}
@@ -25,18 +29,19 @@ interface Props {}
 export const SellersList = () => {
   // TODO: (Future) make this more responsive. Add more than just two widths
   const router = useRouter();
+  const { sendAlert, sendError } = useAlert();
+  const { user } = useAuth();
   const {
     sellers,
     filter,
     updateSellers,
     deleteSeller,
     setSelectedSeller,
+    getAllTransactionsForSeller,
+    getAllLinksForSeller,
   } = useSellers();
 
   useEffect(() => {
-    console.log("YARDSALES: ");
-    console.log(sellers);
-    console.log("Updating YardSales");
     (async () => {
       await updateSellers();
     })();
@@ -62,157 +67,185 @@ export const SellersList = () => {
           textAlign="right"
           className="mobile-my8 pr0"
         >
-          {/* <YardsaleDetailsM`odal /> */}
+          {/* <YardsaleDetailsModal /> */}
         </Grid.Column>
       </Grid.Row>
-      {sellers && sellers?.length == 0 && <div>No Yard Sales</div>}
-      {sellers && sellers?.length > 0 && (
-        <>
-          {sellers
-            ?.filter(
-              (seller: SellersInterface) =>
-                filter.searchText.length === 0 ||
-                seller?.name?.toLowerCase().includes(filter.searchText) ||
-                seller?.initials?.toLowerCase().includes(filter.searchText) ||
-                seller?.company?.toLowerCase().includes(filter.searchText),
-            )
-            .map((seller: SellersInterface) => (
-              <Segment
-                key={seller?.uuid}
-                textAlign="left"
-                fluid="true"
-                compact
-                style={{ border: "0px !important", width: "100%" }}
-                // onClick={(e)=> {
-                //   setSelectedSeller(seller)
-                //   router.push('/yardsales/edit')
-                // }}
-              >
-                <Grid>
-                  <Grid.Row>
-                    <Grid.Column mobile={8} tablet={8} computer={10}>
-                      <Grid.Row verticalAlign="top" className="pb0">
-                        <Grid.Column
-                          computer={6}
-                          mobile={16}
-                          verticalAlign="top"
-                        >
-                          <Card.Content>
-                            <Card.Header as="h3" className="mb0">
-                              {seller?.name} &nbsp; ({seller?.initials})
-                            </Card.Header>
-                            <Card.Meta as="h6">{seller?.company}</Card.Meta>
-                            {/* <Card.Header as="h5" className="m0"><strong>Status:</strong> {(seller?.is_active === true) && (<Icon name="genderless" title="Active" className="active-icon" color="green"></Icon>)}{(seller?.is_active === false) && (<Icon name="genderless" color="red" title="Inactive" className="deactivated-icon"></Icon>)}</Card.Header> */}
-                          </Card.Content>
-                        </Grid.Column>
-                      </Grid.Row>
-                      <Grid.Row className="py0">
-                        <Grid.Column computer={10} mobile={16}>
-                          <Card.Content>
+      <div style={{ overflowY: "auto", height: "65vh" }}>
+        {sellers && sellers?.length == 0 && <div>No Sellers</div>}
+        {sellers && sellers?.length > 0 && (
+          <>
+            {sellers
+              ?.filter(
+                (seller: SellersInterface) =>
+                  filter.searchText.length === 0 ||
+                  seller?.name?.toLowerCase().includes(filter.searchText) ||
+                  seller?.initials?.toLowerCase().includes(filter.searchText) ||
+                  seller?.company?.toLowerCase().includes(filter.searchText),
+              )
+              .map((seller: SellersInterface) => (
+                <Segment
+                  key={seller?.uuid}
+                  textAlign="left"
+                  fluid
+                  compact
+                  style={{ border: "0px !important", width: "100%" }}
+                  // onClick={(e)=> {
+                  //   setSelectedSeller(seller)
+                  //   router.push('/yardsales/edit')
+                  // }}
+                >
+                  <Grid>
+                    <Grid.Row>
+                      <Grid.Column mobile={8} tablet={12} computer={12}>
+                        <Grid.Row verticalAlign="top" className="pb0">
+                          <Grid.Column
+                            computer={12}
+                            tablet={12}
+                            mobile={16}
+                            verticalAlign="top"
+                          >
                             <Card.Content>
-                              {seller.phone && (
-                                <Card.Description>
-                                  <strong>Phone: </strong>
-                                  {seller.phone}
-                                </Card.Description>
-                              )}
-                              {seller.email && (
-                                <Card.Description>
-                                  <strong>Email: </strong>
-                                  {seller.email}
-                                </Card.Description>
-                              )}
-                              {seller.notes && seller.notes.length > 1 && (
-                                <Card.Description>
-                                  <strong>Notes: </strong>
-                                  {String(seller.notes)
-                                    .split("\n")
-                                    .map((line) => (
-                                      <Fragment>
-                                        {line} <br />
-                                      </Fragment>
-                                    ))}
-                                </Card.Description>
-                              )}
+                              <Card.Header as="h3" style={{ marginBottom: 0 }}>
+                                {seller?.name} &nbsp; ({seller?.initials}){" "}
+                              </Card.Header>
+                              <Card.Meta>
+                                {seller?.company && (
+                                  <span>{seller?.company}</span>
+                                )}
+                              </Card.Meta>
                             </Card.Content>
-                          </Card.Content>
-                        </Grid.Column>
-                      </Grid.Row>
-
-                      <Grid.Row className="pt0">
-                        <Grid.Column computer={10} mobile={16}>
-                          <Card.Content>
-                            {seller?.notes && seller?.notes?.length > 1 && (
-                              <Card.Description>
-                                <strong>Notes: </strong>
-                                {String(seller?.notes)
-                                  .split("\n")
-                                  .map((line, index) => (
-                                    <Fragment key={`line2${index}`}>
-                                      {line} <br />
-                                    </Fragment>
-                                  ))}
-                              </Card.Description>
-                            )}
-                          </Card.Content>
-                        </Grid.Column>
-                      </Grid.Row>
-                    </Grid.Column>
-                    <Grid.Column
-                      mobile={8}
-                      tablet={8}
-                      computer={6}
-                      textAlign="right"
-                      verticalAlign="middle"
-                    >
-                      {/* <YardsaleActionCard
-                  seller={seller}
-                  setCashierActive={setCashierActive}
-                /> */}
-                      <Link href="/transactions" as="/cashier">
-                        <Button
-                          className="my-2"
-                          primary
-                          onClick={() => {
-                            setSelectedSeller(seller);
-                          }}
-                        >
-                          Transactions
-                        </Button>
-                      </Link>
-                      <Link
-                        href="/sellers/edit"
-                        as="/sellers/edit"
-                        scroll={true}
+                          </Grid.Column>
+                        </Grid.Row>
+                        <Grid.Row className="py0">
+                          <Grid.Column computer={12} tablet={12} mobile={16}>
+                            <Card.Content>
+                              <Card.Content>
+                                {seller.phone && (
+                                  <Card.Description>
+                                    <strong>Phone: </strong>
+                                    {seller.phone}
+                                  </Card.Description>
+                                )}
+                                {seller.email && (
+                                  <Card.Description>
+                                    <strong>Email: </strong>
+                                    {seller.email}
+                                  </Card.Description>
+                                )}
+                                {seller.notes && seller.notes.length > 1 && (
+                                  <Segment>
+                                    <Card.Description>
+                                      <strong>Notes: </strong> <br />
+                                      {String(seller.notes)
+                                        .split("\n")
+                                        .map((line) => (
+                                          <Fragment key={line}>
+                                            {line} <br />
+                                          </Fragment>
+                                        ))}
+                                    </Card.Description>
+                                  </Segment>
+                                )}
+                              </Card.Content>
+                            </Card.Content>
+                          </Grid.Column>
+                        </Grid.Row>
+                      </Grid.Column>
+                      <Grid.Column
+                        mobile={8}
+                        tablet={4}
+                        computer={4}
+                        textAlign="right"
+                        verticalAlign="middle"
                       >
-                        <Button
-                          className="my-2"
-                          secondary
-                          onClick={() => {
-                            setSelectedSeller(seller);
-                          }}
-                        >
-                          Edit
-                        </Button>
-                      </Link>
-                      <Button
-                        className="my-2"
-                        color="red"
-                        basic
-                        icon="trash"
-                        onClick={async () => {
-                          await deleteSeller(seller.uuid);
-                          await updateSellers();
-                          router.push("/yardsales");
-                        }}
-                      ></Button>
-                    </Grid.Column>
-                  </Grid.Row>
-                </Grid>
-              </Segment>
-            ))}
-        </>
-      )}
+                        <div className="flex col align-stretch wrap">
+                          <div
+                            className="flex row justify-between"
+                            style={{ margin: ".75rem" }}
+                          >
+                            <Link
+                              href="/sellers/edit"
+                              as="/sellers/edit"
+                              scroll={true}
+                            >
+                              <Button
+                                className="my-2"
+                                secondary
+                                fluid
+                                onClick={() => {
+                                  setSelectedSeller(seller);
+                                }}
+                              >
+                                Edit
+                              </Button>
+                            </Link>
+                          </div>
+                          <div
+                            className="flex row justify-around"
+                            style={{ margin: ".75rem" }}
+                          >
+                            <SellerTransactionModal seller={seller} />
+                            {/* <Button
+                              className="my-2"
+                              color="red"
+                              basic
+                              style={{ marginLeft: 16 }}
+                              icon="trash"
+                              circular
+                              onClick={async () => {
+                                router.push("/sellers");
+                              }}
+                            ></Button> */}
+
+                            <ConfirmModal
+                              buttonProps={{
+                                negative: true,
+                                circular: true,
+                                basic: true,
+                                icon: "trash",
+                              }}
+                              header="Confirm Delete"
+                              content={
+                                "Are you sure you want to delete this Seller?"
+                              }
+                              handleConfirm={async () => {
+                                const activeTransactions = await getAllTransactionsForSeller(
+                                  seller.uuid,
+                                );
+                                const activeLinks = await getAllLinksForSeller(
+                                  seller.uuid,
+                                );
+                                if (activeLinks?.data?.length > 0) {
+                                  sendError(
+                                    `Cannot delete ${seller.name} because they are linked to ${activeLinks.data.length} Yard Sale(s).`,
+                                  );
+                                } 
+                                if (activeTransactions?.data?.length > 0) {
+                                  sendError(
+                                    `Cannot delete ${seller.name} because there are ${activeTransactions.data.length} Active Transaction(s).`,
+                                  );
+                                } 
+                                if (activeTransactions?.data?.length === 0 && activeLinks?.data?.length === 0) {
+                                  // await deleteSeller(seller.uuid);
+                                  // await updateSellers();
+
+                                  // router.push("/yardsales");
+                                  sendAlert(
+                                    "Success! Yard Sale has been deleted",
+                                  );
+                                }
+                              }}
+                            />
+                          </div>
+                        </div>
+                      </Grid.Column>
+                    </Grid.Row>
+                  </Grid>
+                </Segment>
+              ))}
+          </>
+        )}
+      </div>
     </>
   );
 };
