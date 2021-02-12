1048ac1b9ec73f6775053179181f45a45e9e2b9a
deric92 <dericgf@gmail.com>
2021-02-11 18:10:19 -0700

    Fullt implemetn Sellers filter form

--- components/Sellers/SellersFilterForm.tsx
+++ components/Sellers/SellersFilterForm.tsx
@@ -1,17 +1,26 @@
 import Link from "next/link";
 import { ChangeEvent, useEffect, useRef } from "react";
-import { Button, Form, Input } from "semantic-ui-react";
+import { Button, Dropdown, Form, Input } from "semantic-ui-react";
 import useForm, { FormValues } from "../../hooks/useForm";
 import { useSellers } from "../../hooks/useSeller";
 import { useYardsales } from "../../hooks/useYardsales";
+import { SellerSortBy } from "../../types/Context";
+import { SellersSortByOptions } from "../../types/DropdownOptions";
 
 export const SellersFilterForm = () => {
-  const { filter, updateFilterText } = useSellers();
+  const { filter, setFilter, updateFilterText } = useSellers();
   const ref = useRef<HTMLInputElement>();
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    console.log(filter.searchText);
-    console.log(e.target.value);
-    updateFilterText(e.target.value);
+  const handleChange = (
+    e: ChangeEvent<HTMLInputElement | HTMLSelectElement>,
+  ) => {
+    setFilter({
+      searchText: e.target.value,
+      sortBy: {
+        key: e.target.innerText,
+        text: e.target.innerText,
+        value: e.target.value as SellerSortBy,
+      },
+    });
   };
 
   useEffect(() => {
@@ -22,18 +31,46 @@ export const SellersFilterForm = () => {
     <Form>
       <Form.Group inline className="mb0">
         <Link href="/sellers/new" as="/sellers/new">
-          <Button>New</Button>
+          <Button color="blue" style={{marginTop: 16}}>New</Button>
         </Link>
-        <Form.Field>
+        <Form.Field style={{marginTop: 16}}>
           <Input
             placeholder="Search"
             icon="search"
-            size="small"
+            
             focus
             iconPosition="left"
             value={filter.searchText}
-            onChange={handleChange}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              setFilter({ ...filter, searchText: e.target.value });
+            }}
             ref={ref}
+          />
+        </Form.Field>
+
+        <Form.Field style={{marginTop: 16}}>
+          <Dropdown
+            closeOnChange
+            icon="filter"
+            floating
+            labeled
+            basic
+            button
+            name="sortBy"
+            text="Sort"
+            className="icon"
+            onChange={async (e: React.SyntheticEvent, data: object) => {
+              setFilter({
+                ...filter,
+                sortBy: {
+                  key: e.target.innerText,
+                  text: e.target.innerText,
+                  value: data.value as SellerSortBy,
+                },
+              });
+            }}
+            options={SellersSortByOptions}
+            value={filter.sortBy.value}
           />
         </Form.Field>
       </Form.Group>
