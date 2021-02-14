90bf2c94ef817c805e843bbe2632efa660ff4744
deric92 <dericgf@gmail.com>
2021-02-13 21:51:18 -0700

    Add HasuraQueryResponse interface

--- types/GraphQL.ts
+++ types/GraphQL.ts
@@ -1,5 +1,19 @@
 export interface UseQueryResponse {
-  loading?: any,
-  error?: any,
-  data: any,
+  loading?: any;
+  error?: any;
+  data: any;
 }
+
+export interface HasuraQueryResponse {
+  data?: {
+    [name: string]: any;
+  };
+  errors?: Array<HasuraErrorObject>;
+}
+
+interface HasuraErrorObject {
+  message: HasuraJWTExpiredErrorMessage;
+  extensions?: any;
+}
+
+type HasuraJWTExpiredErrorMessage = 'Could not verify JWT: JWTExpired'
