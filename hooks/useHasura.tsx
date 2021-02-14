61383f754cdf48078294d0510581c8aa732d0ad0
deric92 <dericgf@gmail.com>
2021-02-13 21:50:27 -0700

    Major improvement to useHasura query function. Now retries correctly if token is expired

--- hooks/useHasura.tsx
+++ hooks/useHasura.tsx
@@ -2,23 +2,12 @@ import { useState, useContext, createContext } from "react";
 import { useToasts } from "react-toast-notifications";
 
 import React from "react";
-import {
-  ApolloClient,
-  ApolloClientOptions,
-  ApolloProvider,
-  HttpLink,
-  InMemoryCache,
-  split,
-} from "@apollo/client";
 import { HasuraContextInterface } from "../types/Context";
-import { useQuery } from "@apollo/client";
 import jwtDecode from "jwt-decode";
-import { getMainDefinition } from "@apollo/client/utilities";
-import { setContext } from "apollo-link-context";
-import { RequestOptions } from "https";
 import { FormValues } from "./useForm";
 import { useAuth } from "./useAuth";
 import { JWTToken } from "../types/JWT";
+import { HasuraQueryResponse } from "../types/GraphQL";
 
 export const HasuraContext = createContext<HasuraContextInterface>(
   {} as HasuraContextInterface,
@@ -28,6 +17,27 @@ interface GraphQlRequestBody {
   variables?: [];
 }
 
+const tryFetch = async (token, query, variables?): Promise<Response> => {
+  let jwt = jwtDecode(token) as JWTToken;
+  let requestBody: GraphQlRequestBody = {
+    query,
+  };
+  if (variables) requestBody.variables = variables;
+
+  let fetchOptions = {
+    method: "POST",
+    headers: {
+      Authorization: token ? `Bearer ${token}` : "",
+      "X-Hasura-Role": jwt["https://hasura.io/jwt/claims"]["x-hasura-role"],
+      "X-Hasura-User-Id":
+        jwt["https://hasura.io/jwt/claims"]["x-hasura-user-id"],
+    },
+    body: JSON.stringify(requestBody),
+  };
+
+  return await fetch(process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT, fetchOptions);
+};
+
 export default function HasuraProvider({ children }) {
   const [context, setContext] = useState();
   const { isTokenExpired, refreshNewAccessToken, refreshToken } = useAuth();
@@ -35,89 +45,32 @@ export default function HasuraProvider({ children }) {
   const query = async (q: string, variables?: any): Promise<any> => {
     let token = localStorage?.getItem("accessToken");
     if (token === null) {
-      return null
+      return null;
     }
-    let jwt = jwtDecode(token) as JWTToken;
-    let requestBody: GraphQlRequestBody = {
-      query: q,
-    };
-    if (variables) requestBody.variables = variables;
 
-    let fetchOptions = {
-      method: "POST",
-      headers: {
-        Authorization: token ? `Bearer ${token}` : "",
-        "X-Hasura-Role": jwt["https://hasura.io/jwt/claims"]["x-hasura-role"],
-        "X-Hasura-User-Id":
-          jwt["https://hasura.io/jwt/claims"]["x-hasura-user-id"],
-      },
-      body: JSON.stringify(requestBody),
-    };
-    let response: Response;
     try {
-      response = await fetch(
-        process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT,
-        fetchOptions,
-      );
-      let { data } = await response.json();
-      
-      // if (data.errors) {
-      //   console.log("Something went wrong with the query...");
-      //   console.log(data.errors);
-      //   if (data?.errors[0]?.message.includes("JWTExpired")) {
-      //     console.log("JWT Was Expired...");
-      //   }
-      // }
-      return data;
+      let response: Response;
+      response = await tryFetch(token, q, variables ?? null);
+      let data: HasuraQueryResponse = await response.json();
+      // Check if the token was expired
+      if (data.errors) {
+        if (data?.errors[0]?.message === 'Could not verify JWT: JWTExpired') {
+          // Token is expired. request a new one and then try the request again.
+          // Only retries once. Might need to add more logic here if second attempt fails...
+          const newToken = await refreshNewAccessToken(refreshToken);
+          response = await tryFetch(newToken, q, variables ?? null);
+          data = await response.json();
+        }
+      }
+      return data.data;
     } catch (error) {
       console.log("Error with query");
       console.log(error);
-      if (await isTokenExpired(token)) {
-        console.log("Token was expired when we tried to query hasura");
-        await refreshNewAccessToken(refreshToken);
-        return await query(q, variables)
-      }
     }
   };
 
   const mutation = async (m: string, variables?: any): Promise<any> => {
-    let token = localStorage?.getItem("accessToken");
-    if (token === null) {
-      return null
-    }
-    let jwt = jwtDecode(token);
-    let requestBody = {
-      query: m,
-    } as FormValues;
-    if (variables) requestBody.variables = variables;
-    let fetchOptions = {
-      method: "POST",
-      headers: {
-        Authorization: token ? `Bearer ${token}` : "",
-        "X-Hasura-Role": jwt["https://hasura.io/jwt/claims"]["x-hasura-role"],
-        "X-Hasura-User-Id":
-          jwt["https://hasura.io/jwt/claims"]["x-hasura-user-id"],
-      },
-      body: JSON.stringify(requestBody),
-    };
-    try {
-      let response = await fetch(
-        process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT,
-        fetchOptions,
-      );
-      let data = await response.json();
-      if (data?.errors[0]?.message.includes("JWTExpired")) {
-        console.log("JWT Was Expired...");
-      }
-      return data;
-    } catch (error) {
-      console.log("Error with query");
-      console.log(error);
-      if (await isTokenExpired(token)) {
-        console.log("Token was expired when we tried to query hasura");
-        await refreshNewAccessToken(refreshToken);
-      }
-    }
+    return await query(m, variables ?? null)
   };
 
   return (
@@ -131,57 +84,3 @@ export const useHasura = () => {
   const ctx = useContext<HasuraContextInterface>(HasuraContext);
   return ctx;
 };
-
-// const httpLink = new HttpLink({
-//   uri: process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT,
-// });
-
-// // const wsLink = new WebSocketLink({
-// // 	uri: process.env.NEXT_PUBLIC_GRAPHQL_WS_ENDPOINT,
-// // 	options: {
-// // 		reconnect: true,
-// // 		connectionParams: {
-// // 			headers: {
-// // 				Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
-// // 			},
-// // 		},
-// // 	},
-// // });
-
-// const link = split(
-//   // split based on operation type
-//   ({ query }) => {
-//     const definition = getMainDefinition(query);
-//     return (
-//       definition.kind === "OperationDefinition" &&
-//       definition.operation === "subscription"
-//     );
-//   },
-//   // wsLink,
-//   httpLink,
-// );
-
-// const authLink = setContext((_, { headers }) => {
-//   const token = localStorage.getItem("accessToken");
-
-//   let jwt = jwtDecode(token);
-//   console.log("JWT: ");
-//   console.log(jwt);
-//   // TODO: IMPORTANT: check if token is expired here. If it is -> use refresh token to get another.
-//   return {
-//     headers: {
-//       ...headers,
-//       Authorization: token ? `Bearer ${token}` : "",
-//       "X-Hasura-Role": jwt["https://hasura.io/jwt/claims"]["x-hasura-role"],
-//       "X-Hasura-User-Id":
-//         jwt["https://hasura.io/jwt/claims"]["x-hasura-user-id"],
-//     },
-//   };
-// });
-
-// const appClient = new ApolloClient({
-//   link: authLink.concat(link),
-//   cache: new InMemoryCache(),
-// });
-
-// const [client, setClient] = useState(appClient);
